62e9703b31c483a8524af5b89c3d2080cd6df941
realraum <realraum@realraum.at>
2010-07-28 01:21:33 +0000

    better Panic ?

--- switch-power.py
+++ switch-power.py
@@ -211,7 +211,9 @@ def eventPanic():
   logging.info("eventPanic(): switching around: "+uwscfg.slug_ids_panic)
   lst1 = uwscfg.slug_ids_panic.split(" ")
   lst2 = map(lambda e:[e,True], lst1)
-  for delay in map(lambda e: (40-e)/133.0,range(0,20)):
+  for id in lst1:
+    switchPower(id,True)
+  for delay in map(lambda e: (40-e)/33.0,range(0,20)):
     e = random.choice(lst2)
     e[1]=not e[1]
     switchPower(e[0],e[1]) 
